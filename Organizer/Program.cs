5b8905f21094c03d952a4643293bf651cc889aa7
Ramon van Opoven <rvophoven@gmail.com>
2022-11-21 10:28:21 +0100

    intro e

--- Organizer/Program.cs
+++ Organizer/Program.cs
@@ -18,14 +18,32 @@ namespace Organizer
 
             int amount = Convert.ToInt32(Console.ReadLine());//get number of objects in list
 
-            if (amount > 100)// number bigger than 100 display error
+            List<int> randomList = MakeList(amount);
+            stopWatch.Start();
+            Console.WriteLine(stopWatch.Elapsed);//test time
+            List<int> sortList = mySort.Sort(randomList);
+            Console.WriteLine(stopWatch.Elapsed);
+            stopWatch.Stop();
+
+            Console.WriteLine("First sorted and second unsorted list:");
+
+            for (int x = 0; x < sortList.Count && x < 50 ; x++)
             {
-                Console.WriteLine("List is to long. List is > 100.");
+                Console.Write(sortList[x]);
+                Console.Write(" , ");
+                Console.WriteLine(randomList[x]);
             }
-            else//else make and show list
+
+            List<int> sortList2 = mySort.SortFunction(randomList);
+
+            Console.WriteLine("Sorted list:");
+
+            for (int x = 0; x < sortList.Count && x < 50; x++)
             {
-                MakeList(amount);
+                Console.WriteLine(sortList2[x]);
             }
+
+
 
         }
 
@@ -36,29 +54,17 @@ namespace Organizer
         /// </summary>
         /// <param name="label">The label for this list</param>
         /// <param name="theList">The list to show</param>
-        public static void MakeList(int amount)//(string label, List<int> theList)
+        public static List<int> MakeList(int amount)//(string label, List<int> theList)
         {
             Random randomNumber = new Random();
             List<int> randomList = new List<int>();
 
-            Console.WriteLine("Unsorted list:");
             for (int x = 0; x < amount; x++)
             {
                 randomList.Add(randomNumber.Next(-99, 99));// returns random integers >= -99 and < 99
-                Console.WriteLine(randomList[x]);// print random list
             }
 
-            stopWatch.Start();
-            Console.WriteLine(stopWatch.Elapsed);//test time
-            List<int> sortList = mySort.Sort(randomList); //sort random list
-            Console.WriteLine(stopWatch.Elapsed);
-            stopWatch.Stop();
-
-            Console.WriteLine("Sorted list:");
-            foreach (var num in sortList)// print sorted list
-            {
-                Console.WriteLine(num);
-            }
+            return randomList;
 
         }
 
--- Organizer/ShiftHigestSort.cs
+++ Organizer/ShiftHigestSort.cs
@@ -6,6 +6,7 @@ namespace Organizer
 	public class ShiftHighestSort
     {
         private List<int> array = new List<int>();
+        private List<int> randomList = new List<int>();
 
         /// <summary>
         /// Sort an array using the functions below
@@ -28,11 +29,29 @@ namespace Organizer
         /// </summary>
         /// <param name="low">De index within this.array to start with</param>
         /// <param name="high">De index within this.array to stop with</param>
-        private void SortFunction(int low, int high)
+        public List<int> SortFunction(List<int> input)  //int low, int high
         {
-            throw new NotImplementedException();
-            // list [0,x] unsort
-            // list [1,x] sort?
+            randomList = new List<int>(input);
+            List<int> sortList = new List<int>();
+
+            int i = 1;
+
+            while (i < array.Count +1)
+            {
+                for (int x = 0; x < randomList.Count - i; x++)
+                {
+                    int indexA = randomList[x];
+                    int indexB = randomList[x+1];
+
+                    if (indexA > indexB)
+                    {
+                        randomList[x] = indexB;
+                        randomList[x + 1] = indexA;
+                    }
+                }
+                i++;
+            }
+            return randomList;
         }
     }
 }
