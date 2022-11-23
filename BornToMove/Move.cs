0c0dee86e272498cbdd4fcbbf7198671bd63a647
Ramon van Opoven <rvophoven@gmail.com>
2022-11-23 16:49:42 +0100

    a t/m g

--- BornToMove/Move.cs
+++ BornToMove/Move.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,16 +10,93 @@ namespace BornToMove
 {
     public class Move
     {
-        public string id { get; set; }
-        public string name { get; set; }
-        public string description { get; set; }
-        public string sweatrate { get; set; }
+        public int[] id { get; set; }
+        public string[] name { get; set; }
+        public string[] description { get; set; }
+        public int[] sweatrate { get; set; }
+
+        private string source = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\rvoph\\source\\repos\\educom-csharp-algorithm\\BornToMove\\born2move.mdf;Integrated Security=True";
 
 
-        public void ID()
+        public void getExerColm(string name)
         {
-            Console.WriteLine("ID");
+
+            SqlConnection conn = new SqlConnection(source);
+
+            string sql = "select " + name + " from exercises";
+            
+            SqlCommand cmd = new SqlCommand(sql, conn);
+
+            conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine("{0}", reader.GetString(0));  
+                }
+            }else{Console.WriteLine("No data found");}
+            conn.Close();
+
         }
+
+        public void getExerRow(int id)
+        {
+            SqlConnection conn = new SqlConnection(source);
+
+            string sql = "select name, description, sweatrate from exercises where id=" + id;
+
+            SqlCommand cmd = new SqlCommand(sql, conn);
+
+            conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine("name: {0}",reader.GetString(0));
+                    Console.WriteLine("Description: {0}",reader.GetString(1));
+                    Console.WriteLine("Sweatrate: {0}",reader.GetInt32(2));
+                }
+            }
+            else { Console.WriteLine("No data found"); }
+            conn.Close();
+
+        }
+
+        public void setExerRow(string name, string descript, int rate)
+        {
+            SqlConnection conn = new SqlConnection(source);
+            //change to insert.............
+            string sql = "select name, description, sweatrate from exercises where";
+            //change
+            SqlCommand cmd = new SqlCommand(sql, conn);
+
+            conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            conn.Close();
+
+        }
+
+        public void setRatingRow(int score, int rate)
+        {
+            SqlConnection conn = new SqlConnection(source);
+            //change to insert.............
+            string sql = "select name, description, sweatrate from exercises where";
+            //change
+            SqlCommand cmd = new SqlCommand(sql, conn);
+
+            conn.Open();
+            
+            conn.Close();
+
+        }
+
+
+
 
 
 
--- BornToMove/Program.cs
+++ BornToMove/Program.cs
@@ -1,44 +1,126 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BornToMove
 {
-    internal class Program
+    public class Program
     {
+        private static Move myMove = new Move();
+        
 
-        private static Move myID = new Move ();
-
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+
             Console.WriteLine("Get moving!!!!!!!!!!!");
+
             Console.WriteLine("");
             Console.WriteLine("Do you want a suggestion or to choose?");
             Console.WriteLine("1: Suggestion");
             Console.WriteLine("2: choose");
             int choice = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("");
-            Console.WriteLine("What exercise do you wanna do?");
-            
+            while(choice != 1 && choice != 2)
+            {
+                Console.WriteLine("invalid number: {0}! choose again ", choice);
+                choice = Convert.ToInt32(Console.ReadLine());
+            }
 
-            //placeholders get database
-            Console.WriteLine("0: create");
-            Console.WriteLine("1: Push up");
-            Console.WriteLine("2: Planking");
-            Console.WriteLine("3: Squat");
-            Console.WriteLine("Type the number for more information.");
-            choice = Convert.ToInt32(Console.ReadLine());
-            myID.ID();
+            if(choice == 1)
+            {
+                suggestion();
+            }else
+            {
+                choose();
+            }
+            Console.WriteLine("Finished? Then press enter.");
+            Console.ReadLine();
+            rating();
 
-            Console.WriteLine("What dit you thing of the exercise:");
-            string comment = Console.ReadLine();
-            Console.Write("And from 1-5 how intense was it:");
-            int score = Convert.ToInt32(Console.ReadLine());
-
-
+            Console.WriteLine("Thank you for the hard work");
             Console.ReadLine();//hold end
         }
+
+        private static void suggestion()
+        {
+            //get ids and choose a random one
+            int choice = 1;//random id
+            myMove.getExerRow(choice);
+        }
+
+        private static void choose()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("What exercise do you wanna do?");
+
+            Console.WriteLine("0: create exercise");
+            //get exercises from database....
+            Console.WriteLine("Type the number for more information.");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            if(choice == 0)
+            {
+                addexer();
+            }
+            else
+            {
+                //show choice
+                myMove.getExerRow(choice);
+            }
+        }
+
+        private static void addexer()
+        {
+            Console.WriteLine("Give it a name:");
+            string name = Console.ReadLine();
+            //check name in database
+            Console.WriteLine("Give it a discription:");
+            string description = Console.ReadLine();
+            Console.WriteLine("Give it a sweatrate:");
+            int sweatrate = Convert.ToInt32(Console.ReadLine());
+
+            //send to database
+        }
+
+            private static void rating()
+        {
+            Console.WriteLine("From 1-5 what dit you thing of the exercise?");
+            int rating = Convert.ToInt32(Console.ReadLine());
+            Console.Write("And from 1-5 how intense was it?");
+            int score = Convert.ToInt32(Console.ReadLine());
+
+            //send to database...
+        }
+
+
+        private static void connect()//old connect methode
+        {
+            SqlConnection conn = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\rvoph\\source\\repos\\educom-csharp-algorithm\\BornToMove\\born2move.mdf;Integrated Security=True");
+
+            string sql = "select id, name, description, sweatrate from exercises";
+            SqlCommand cmd = new SqlCommand(sql, conn);
+
+            conn.Open();
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine("{0} {1} {2} {3}", reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetInt32(3));
+                }
+            }
+            else
+            {
+                Console.WriteLine("No data found");
+            }
+            conn.Close();
+
+
+        }
+
     }
 }
